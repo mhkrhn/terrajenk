c577c7e36075487b00a74d8c9dc7357f4064f1fa
mhkrhn <128690942+mhkrhn@users.noreply.github.com>
2023-06-08 15:24:35 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -128,3 +128,18 @@ resource "azurerm_linux_virtual_machine" "nginx" {
 
    depends_on = [azurerm_resource_group.webserver]
 }
+resource "azurerm_virtual_network" "webserver-net" {
+  name                = "webserver-net"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.webserver.location
+  resource_group_name = azurerm_resource_group.webserver.name
+}
+
+resource "azurerm_subnet" "webserver-subnet" {
+  name                 = "subnet01"
+  resource_group_name  = azurerm_resource_group.webserver.name
+  virtual_network_name = azurerm_virtual_network.webserver-net.name
+  address_prefixes       = ["10.0.1.0/24"]
+
+  private_link_service_network_policies_enabled = false
+}
