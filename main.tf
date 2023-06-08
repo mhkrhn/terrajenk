b456222c97c92005db6a978b26da5cb6694b81ad
mhkrhn <128690942+mhkrhn@users.noreply.github.com>
2023-06-08 16:42:50 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,163 +1,22 @@
 terraform {
-   required_version = ">= 0.12"
-   required_providers {
-      azurerm = ">3.0"
-   }
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">3.0"
+    }
+  }
 }
 
 provider "azurerm" {
-   subscription_id = var.subscription_id
-   #client_id = var.client_id
-   #client_secret = var.client_secret
-   tenant_id = var.tenant_id
+   subscription_id = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
+   client_id = "c83c3362-163d-44ec-9d5a-0dabe5845984"
+   client_secret = "43824064-bc3d-45a0-935b-b6fc3a4593f4"
+   tenant_id = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
    skip_provider_registration = true
    features {}
 }
-variable "subscription_id" {
-   description = "Azure subscription"
-   default = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
-}
-variable "tenant_id" {
-   description = "Azure Tenant ID"
-   default = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
-}
-variable "location" {
-   type = string
-   description = "Region"
-   default = "France Central"
-}
-variable "environment" {
-   type = string
-   description = "Environment"
-   default = "Staging"
-}
-variable "instance_size" {
-   type = string
-   description = "Azure instance size"
-   default = "Standard_F2"
-}
-resource "azurerm_resource_group" "webserver" {
-   name = "mktestgroup"
-   location = var.location
-}
 
-resource "azurerm_network_security_group" "allowedports" {
-   name = "allowedports"
-   resource_group_name = azurerm_resource_group.webserver.name
-   location = azurerm_resource_group.webserver.location
-  
-   security_rule {
-       name = "http"
-       priority = 100
-       direction = "Inbound"
-       access = "Allow"
-       protocol = "Tcp"
-       source_port_range = "*"
-       destination_port_range = "80"
-       source_address_prefix = "*"
-       destination_address_prefix = "*"
-   }
-
-   security_rule {
-       name = "https"
-       priority = 200
-       direction = "Inbound"
-       access = "Allow"
-       protocol = "Tcp"
-       source_port_range = "*"
-       destination_port_range = "443"
-       source_address_prefix = "*"
-       destination_address_prefix = "*"
-   }
-
-   security_rule {
-       name = "ssh"
-       priority = 300
-       direction = "Inbound"
-       access = "Allow"
-       protocol = "Tcp"
-       source_port_range = "*"
-       destination_port_range = "22"
-       source_address_prefix = "*"
-       destination_address_prefix = "*"
-   }
-}
-
-resource "azurerm_public_ip" "webserver_public_ip" {
-   name = "mkwebserver_public_ip"
-   location = var.location
-   resource_group_name = azurerm_resource_group.webserver.name
-   allocation_method = "Dynamic"
-
-   tags = {
-       environment = var.environment
-       costcenter = "it"
-   }
-
-   depends_on = [azurerm_resource_group.webserver]
-}
-
-resource "azurerm_network_interface" "webserver" {
-   name = "nginx-interface"
-   location = azurerm_resource_group.webserver.location
-   resource_group_name = azurerm_resource_group.webserver.name
-
-   ip_configuration {
-       name = "internal"
-       private_ip_address_allocation = "Dynamic"
-       subnet_id = azurerm_subnet.webserver-subnet.id
-       public_ip_address_id = azurerm_public_ip.webserver_public_ip.id
-   }
-
-   depends_on = [azurerm_resource_group.webserver]
-}
-   resource "azurerm_linux_virtual_machine" "nginx" {
-   size = var.instance_size
-   name = "nginx-webserver"
-   resource_group_name = azurerm_resource_group.webserver.name
-   location = azurerm_resource_group.webserver.location
-   custom_data = base64encode(file("init-script.sh"))
-   network_interface_ids = [
-       azurerm_network_interface.webserver.id,
-   ]
-      source_image_reference {
-       publisher = "Canonical"
-       offer = "UbuntuServer"
-       sku = "18.04-LTS"
-       version = "latest"
-   }
-
-   computer_name = "mktest"
-   admin_username = "mkadminuser"
-   admin_password = "MKarahan507144"
-   disable_password_authentication = false
-
-   os_disk {
-       name = "mksdisk01"
-       caching = "ReadWrite"
-       #create_option = "FromImage"
-       storage_account_type = "Standard_LRS"
-   }
-
-   tags = {
-       environment = var.environment
-       costcenter = "it"
-   }
-
-   depends_on = [azurerm_resource_group.webserver]
-}
-resource "azurerm_virtual_network" "webserver-net" {
-  name                = "webserver-net"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.webserver.location
-  resource_group_name = azurerm_resource_group.webserver.name
-}
-
-resource "azurerm_subnet" "webserver-subnet" {
-  name                 = "subnet01"
-  resource_group_name  = azurerm_resource_group.webserver.name
-  virtual_network_name = azurerm_virtual_network.webserver-net.name
-  address_prefixes       = ["10.0.1.0/24"]
-
-  private_link_service_network_policies_enabled = false
+resource "azurerm_resource_group" "test" {
+  name     = "MKjenk2"
+  location = "France Central"
 }
