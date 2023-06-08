6e75ca3210e1ed020571e9ffbc1810689a0cb8a7
mhkrhn <128690942+mhkrhn@users.noreply.github.com>
2023-06-08 15:29:18 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+terraform {
+   required_version = ">= 0.12"
+   required_providers {
+      azurerm = ">3.0"
+   }
+}
+
+provider "azurerm" {
+   subscription_id = var.subscription_id
+   #client_id = var.client_id
+   #client_secret = var.client_secret
+   tenant_id = var.tenant_id
+   skip_provider_registration = true
+   features {}
+}
 variable "subscription_id" {
    description = "Azure subscription"
    default = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
