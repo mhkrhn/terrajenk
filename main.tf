3f1caddc6eb18166427de2a07a1d6582cf38cfc6
mhkrhn <128690942+mhkrhn@users.noreply.github.com>
2023-06-09 09:38:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 }
 
-provider "mkjenkins" {
+provider "azurerm" {
    subscription_id = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
    client_id = "c83c3362-163d-44ec-9d5a-0dabe5845984"
    client_secret = "rI58Q~58UQDhWPNW_yG5.cSbP2LD7kpmhAXhTc__"
