3ac4e3fb8f036fa5340893a03b50e6591890476b
mhkrhn <128690942+mhkrhn@users.noreply.github.com>
2023-06-09 10:14:46 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "azurerm" {
    subscription_id = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
-   client_id = "c83c3362-163d-44ec-9d5a-0dabe5845984"
-   client_secret = "rI58Q~58UQDhWPNW_yG5.cSbP2LD7kpmhAXhTc__"
+   client_id = "d824ff95-865a-4dec-9852-bb1e1702f12b"
+   client_secret = "uSs8Q~3uRnaX~ouOEwzdXBqnup-vDAZbbpEPwbTY"
    tenant_id = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
    skip_provider_registration = true
    features {}
